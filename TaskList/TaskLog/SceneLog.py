e63500b4cb1b234f49c91cb23fe261d3b1da9bc5
Captain DesAstres <captaindesastres@gmail.com>
2016-08-02 23:21:56 +0200

    make SceneLog class skeleton based on old GroupLog Class

--- TaskList/TaskLog/SceneLog.py
+++ TaskList/TaskLog/SceneLog.py
@@ -8,7 +8,94 @@ from usefullFunctions import XML
 class SceneLog:
 	'''class to manage task scene log'''
 	
-	
 	def __init__(self, xml = None):
 		'''initialize scene log object'''
 		
+	
+	
+	
+	
+	
+	def defaultInit():
+		'''initialize Scene log'''
+		self.name
+		self.path
+		self.start
+		self.end
+		self.frames
+		self.status
+	
+	
+	
+	
+	
+	def fromXml():
+		'''initialize Scene log object with saved log'''
+		self.name
+		self.path
+		self.start
+		self.end
+		self.frames
+		self.status
+	
+	
+	
+	
+	
+	def toXml():
+		'''export scene log into xml syntaxed string'''
+	
+	
+	def menu():
+		'''see detail of the Scene rendering'''
+		
+	
+	
+	
+	
+	
+	def print():
+		'''A method to print Scene log'''
+	
+	
+	
+	
+	
+	def runMenuPrint():
+		'''display rendering progress during run mode '''
+	
+	
+	
+	
+	
+	def average():
+		'''return frame average rendering time'''
+	
+	
+	
+	
+	
+	def remaining():
+		'''return the count of frames that don't have been rendered yet'''
+	
+	
+	
+	
+	
+	def confirmFrame():
+		'''add frame rendering log confirmation to the scene'''
+	
+	
+	
+	
+	
+	def checkFrames():
+		'''check for each frame that have been claimed as rendered if there is really a file corresponding to it'''
+	
+	
+	
+	
+	
+	
+	
+	
