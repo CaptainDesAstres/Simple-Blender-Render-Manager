dd591b94634ae98871cc60a59d65a839193a2f37
Captain DesAstres <captaindesastres@gmail.com>
2016-08-02 23:14:42 +0200

    create a class to manage scene log

---
+++ TaskList/TaskLog/SceneLog.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage task scene  log'''
+import xml.etree.ElementTree as xmlMod
+from usefullFunctions import XML
+
+
+class SceneLog:
+	'''class to manage task scene log'''
+	
+	
+	def __init__(self, xml = None):
+		'''initialize scene log object'''
+		
