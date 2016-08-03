5f5e7adba31b451c51f8a5cfaa909e5a579b0c0d
Captain DesAstres <captaindesastres@gmail.com>
2016-08-03 21:46:42 +0200

    recode RenderingTask() function

--- TaskList/RenderingTask/RenderingTask.py
+++ TaskList/RenderingTask/RenderingTask.py
@@ -5,38 +5,45 @@ sys.path.append(os.path.abspath(sys.argv[4]+'/../../../..'))
 def RenderingTask(task, preferences):
 	task.running = True
 	
-	#scene = bpy.data.scenes[task.scene]
-	#bpy.context.screen.scene = scene
-	
-	scene.render.use_stamp_time = True
-	scene.render.use_stamp_date = True
-	scene.render.use_stamp_render_time = True
-	scene.render.use_stamp_frame = True
-	scene.render.use_stamp_scene = True
-	scene.render.use_stamp_camera = True
-	scene.render.use_stamp_lens = True
-	scene.render.use_stamp_filename = True
-	scene.render.use_stamp_marker = True
-	scene.render.use_stamp_sequencer_strip = True
-	scene.render.use_stamp_note = True
-	
-	scene.render.use_file_extension = True
-	scene.render.use_placeholder = True
-	
+	# create a socket to communicate with blender-render-manager
 	connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	connexion.connect(('localhost', preferences.port))
-	
 	listen = threading.Thread(
 				target = socketListener,
 				args=(connexion, task) 
 							)
 	listen.start()
 	
-	try:
-		run(task, bpy, connexion, preferences )
-	except Exception as e:
-		connexion.sendall( (task.uid+' debugMsg('+str(e)+') EOS').encode() )
+	scenes = task.log.scenes
+	for scene in scenes:
+		# select scene as active
+		scene = bpy.data.scenes[scene.name]
+		bpy.context.screen.scene = scene
+		
+		# active all stamp info
+		scene.render.use_stamp_time = True
+		scene.render.use_stamp_date = True
+		scene.render.use_stamp_render_time = True
+		scene.render.use_stamp_frame = True
+		scene.render.use_stamp_scene = True
+		scene.render.use_stamp_camera = True
+		scene.render.use_stamp_lens = True
+		scene.render.use_stamp_filename = True
+		scene.render.use_stamp_marker = True
+		scene.render.use_stamp_sequencer_strip = True
+		scene.render.use_stamp_note = True
+		
+		# active output option
+		scene.render.use_file_extension = True
+		scene.render.use_placeholder = True
+		
+		# render scene
+		try:
+			run(task, bpy, connexion, preferences )
+		except Exception as e:
+			connexion.sendall( (task.uid+' debugMsg('+str(e)+') EOS').encode() )
 	
+	# send end of rendering signal
 	task.running = 'NOW'
 	connexion.sendall( (task.uid+' VersionEnded EOS').encode() )
 	
