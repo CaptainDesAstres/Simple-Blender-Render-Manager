b35426ec75ca8c3ca62241a2cffd1e2dcc1498d6
Captain DesAstres <captaindesastres@gmail.com>
2016-08-05 14:35:02 +0200

    add/clarify comment, erase useless instruction, clarify variable name

--- save.py
+++ save.py
@@ -19,29 +19,33 @@ def eraseLockFile():
 	os.remove(os.getcwd()+'/lock')
 
 def checkLogLimit(limit):
-	'''a function to limit the session log number'''
+	'''limit the session log number'''
+	# get log file list
 	content = os.listdir(os.getcwd()+'/log/')
 	
 	if len(content) > limit:
+		
+		# link log to timestamp computed from creation date
 		logRegex = re.compile(r'^session (\d{2})\.(\d{2})\.(\d{4})-(\d{2}):(\d{2}):(\d{2})\.log$')
 		logs = {}
 		for f in content:
 			match = logRegex.match(f)
 			if match is not None:
-				t = int(match.group(3))*12+int(match.group(2))
-				t = t*31+int(match.group(1))
-				t = t*24+int(match.group(4))
-				t = t*60+int(match.group(5))
-				t = t*60+int(match.group(6))
-				logs[t] = f
+				timestamp = int(match.group(3))*12+int(match.group(2)) # month
+				timestamp = timestamp*31+int(match.group(1)) # days (approximation)
+				timestamp = timestamp*24+int(match.group(4)) # hour
+				timestamp = timestamp*60+int(match.group(5)) # minute
+				timestamp = timestamp*60+int(match.group(6)) # second
+				
+				logs[timestamp] = f
 		
-		if len(logs) > limit:
-			keys = list(logs.keys())
-			keys.sort()
-			while len(logs) > limit:
-				k = keys.pop(0)
-				f = logs.pop(k)
-				os.remove(os.getcwd()+'/log/'+f)
+		# erase oldest log(s) until it's match the limit settings
+		keys = list(logs.keys())
+		keys.sort()
+		while len(logs) > limit:
+			k = keys.pop(0)
+			f = logs.pop(k)
+			os.remove(os.getcwd()+'/log/'+f)
 
 
 
